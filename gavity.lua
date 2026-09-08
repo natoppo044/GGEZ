dcf80a276c6beb0415f35ca911c0ba8c4c23c2d5
GGEZ <91177243+natoppo044@users.noreply.github.com>
2026-09-08 18:54:46 +0700

    Create gavity.lua

---
+++ gavity.lua
@@ -0,0 +1,8 @@
+GravityHub = {
+    Key = "",
+    Team = "Pirates",
+    Color = "Red",
+    SaveSetting = false,
+    AutoExecute = false
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-GravityHub/BloxFruit/refs/heads/main/MainPremium.lua"))()
